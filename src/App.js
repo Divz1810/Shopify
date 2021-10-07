d7817833c4bb898346dd4c2f7b8d93debfc38d8c
Divz1810 <divya.mishra1899@gmail.com>
2021-10-08 02:32:55 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,41 +1,95 @@
 import React, { useState, useEffect} from 'react';
 import { commerce } from './lib/commerce';
-import {Products , Navbar , Cart} from './components';
+import {Products , Navbar , Cart, Checkout} from './components';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App=()=> {
 
-  const [products, setProducts] = useState([]);
-  const [cart, setCart]= useState({});
+   const [products, setProducts] = useState([]);
+   const [cart, setCart]= useState({});
+   const[order, setOrder]= useState({});
+   const[errorMessage, setErrorMessage]= useState('');
 
-//fetchProducts is an async function that fetch all the products and update state i.e products
-  const fetchProducts = async () => {
-    const { data }= await commerce.products.list();
+   //fetchProducts is an async function that fetch all the products and update state i.e products
+   const fetchProducts = async () => {
+     const { data }= await commerce.products.list();
 
     setProducts(data);
+   }
+
+   const fetchCart = async() =>{
+     setCart(await commerce.cart.retrieve());
+   }
+
+   const handleAddToCart = async (productId, quantity) =>{
+     const { cart } = await commerce.cart.add(productId, quantity);
+     setCart(cart);
+   }
+
+   const handleUpdateCartQty = async (productId, quantity) =>{
+    const { cart } = await commerce.cart.update(productId, {quantity});
+    setCart(cart);
   }
 
-  const fetchCart = async() =>{
-    setCart(await commerce.cart.retrieve());
+  const handleRemoveFromCart = async (productId) =>{
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
   }
 
-  const handleAddToCart = async (productId, quantity) =>{
-    const response = await commerce.cart.add(productId, quantity);
+  const handleEmptyCart = async () =>{
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
   }
 
-//useEffect will call the function fetchProducts only once. after first render
-  useEffect(()=>{
-    fetchProducts();
+  const refreshCart = async()=>{
+    const newCart= await commerce.cart.refresh();
+    setCart(newCart);
+  }
+
+  const handleCaptureCheckout= async(checkoutTokenId, newOrder)=>{
+    try{
+      const incomingOrder= await commerce.checkout.capture(checkoutTokenId, newOrder);
+      setOrder(incomingOrder);
+      refreshCart();
+    }catch(error){
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
+ //useEffect will call the function fetchProducts only once. after first render
+   useEffect(()=>{
+     fetchProducts();
     fetchCart();
-  }, []);
+   }, []);
 
-  console.log(cart);
+//   console.log(cart);
 
   return (
+    <Router>
     <div>
       <Navbar totalItems= {cart.total_items} />
-      {/* <Products products= {products} onAddToCart={handleAddToCart} /> */}
-      <Cart cart={cart}/>
+      <Switch>
+        <Route exact path="/">
+           <Products products= {products} onAddToCart={handleAddToCart} />
+        </Route>
+        <Route exact path="/cart">
+            <Cart
+               cart={cart} 
+                handleUpdateCartQty= {handleUpdateCartQty}
+                handleRemoveFromCart= {handleRemoveFromCart}
+                handleEmptyCart= {handleEmptyCart}
+               />
+        </Route>
+        <Route exact path= "/Checkout">
+              <Checkout cart= {cart}
+              order={order}
+              onCaptureCheckout={handleCaptureCheckout}
+              error={errorMessage}
+               />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
